a97ed7d4d714b823e08617e892bf6108763dfcf2
Miguel G. Sanguino <sanguino@me.com>
2021-02-20 18:33:08 +0100

    add annotations to entity

--- src/main/java/es/sanguino/ecommerce/infrastructure/entity/ProductEntity.java
+++ src/main/java/es/sanguino/ecommerce/infrastructure/entity/ProductEntity.java
@@ -1,7 +1,15 @@
 package es.sanguino.ecommerce.infrastructure.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class ProductEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Long price;
